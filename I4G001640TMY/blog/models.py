1b9d921734e0d6e74cbd9820cf4e5084dc1b733d
Ogirimah <93133254+Ogirimah@users.noreply.github.com>
2022-06-29 10:50:19 +0100

    blog: edited the author field in the models.py               migrations/0002......: migrate the changes.

--- I4G001640TMY/blog/models.py
+++ I4G001640TMY/blog/models.py
@@ -1,11 +1,13 @@
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth import get_user_model
 
 # Post
 
+
 class blog(models.Model):
-  Title = models.CharField(max_length=30)
-  Text = models.TextField()
-  Author = get_user_model()
-  Created_date = models.DateTimeField()
-  Published_date = models.DateTimeField()
+    Title = models.CharField(max_length=30)
+    Text = models.TextField()
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
